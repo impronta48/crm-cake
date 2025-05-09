910c6263e288f7279f0218a1609b7dd99fae4c1b
Massimo Infunti <massimoi@nkoni.org>
2025-05-09 12:15:26 +0200

    aggiunta config di esempio

--- config/sites/settings.example.php
+++ config/sites/settings.example.php
@@ -6,13 +6,13 @@
 // *** NON INSERIRE DATI SENSIBILI ***
 return [
 
-    'App' => [
-      'base' => '/api',
-      'webroot' => '/path/to/webroot/',
-      'fullBaseUrl' => 'https://example.com',
-      'imageBaseUrl' => 'img/',
-    ],
-    /*
+  'App' => [
+    'base' => '/api',
+    'webroot' => '/path/to/webroot/',
+    'fullBaseUrl' => 'https://example.com',
+    'imageBaseUrl' => 'img/',
+  ],
+  /*
        * Debug Level:
        *
        * Production Mode:
@@ -21,118 +21,129 @@ return [
        * Development Mode:
        * true: Errors and warnings shown.
        */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
-  
-    /*
+  'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+
+  /*
        * Security and encryption configuration
        *
        * - salt - A random string used in security hashing methods.
        *   The salt value is also used as the encryption key.
        *   You should treat it as extremely sensitive data.
        */
-    'Security' => [
-      'salt' => env('SECURITY_SALT', 'your_security_salt_here'),
-    ],
-  
-    /*
+  'Security' => [
+    'salt' => env('SECURITY_SALT', 'your_security_salt_here'),
+  ],
+
+  /*
        * Connection information used by the ORM to connect
        * to your application's datastores.
        *
        * See app.php for more configuration options.
        */
-    'Datasources' => [
-      'default' => [
-        'host' => 'localhost',
-        /*
+  'Datasources' => [
+    'default' => [
+      'host' => 'localhost',
+      /*
                * CakePHP will use the default DB port based on the driver selected
                * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
                * the following line and set the port accordingly
                */
-        //'port' => 'non_standard_port_number',
-  
-        'username' => 'your_db_username',
-        'password' => 'your_db_password',
-  
-        'database' => 'your_database_name',
-        /*
+      //'port' => 'non_standard_port_number',
+
+      'username' => 'your_db_username',
+      'password' => 'your_db_password',
+
+      'database' => 'your_database_name',
+      /*
                * If not using the default 'public' schema with the PostgreSQL driver
                * set it here.
                */
-        //'schema' => 'myapp',
-  
-        /*
+      //'schema' => 'myapp',
+
+      /*
                * You can use a DSN string to set the entire configuration
                */
-        'url' => env('DATABASE_URL', null),
-      ],
-  
-      /*
+      'url' => env('DATABASE_URL', null),
+    ],
+
+    /*
            * The test connection is used during the test suite.
            */
-      'test' => [
-        'host' => 'localhost',
-        //'port' => 'non_standard_port_number',
-        'username' => 'your_test_db_username',
-        'password' => 'your_test_db_password',
-        'database' => 'your_test_database_name',
-        //'schema' => 'myapp',
-        'url' => env('DATABASE_TEST_URL', null),
-      ],
+    'test' => [
+      'host' => 'localhost',
+      //'port' => 'non_standard_port_number',
+      'username' => 'your_test_db_username',
+      'password' => 'your_test_db_password',
+      'database' => 'your_test_database_name',
+      //'schema' => 'myapp',
+      'url' => env('DATABASE_TEST_URL', null),
     ],
-  
-    /*
+  ],
+
+  /*
        * Email configuration.
        *
        * Host and credential configuration in case you are using SmtpTransport
        *
        * See app.php for more configuration options.
        */
-    'EmailTransport' => [
-      'Debug' => [
-        'className' => 'Debug',
-      ],
-      'default' => [
-        'host' => 'localhost',
-        'port' => 25,
-        'username' => null,
-        'password' => null,
-        'client' => null,
-        'url' => env('EMAIL_TRANSPORT_DEFAULT_URL', null),
-      ],
+  'EmailTransport' => [
+    'Debug' => [
+      'className' => 'Debug',
     ],
-  
-    /*
+    'default' => [
+      'host' => 'localhost',
+      'port' => 25,
+      'username' => null,
+      'password' => null,
+      'client' => null,
+      'url' => env('EMAIL_TRANSPORT_DEFAULT_URL', null),
+    ],
+  ],
+
+
+
+  /*
     * WhatsApp Configuration.
     */
-    'WhatsApp' => [
-      'base_url' => 'http://localhost',
-      'port' => 4000,
-      'api_key' => 'your_whatsapp_api_key',
-    ],
-  
-    /*
+  'WhatsApp' => [
+    'base_url' => 'http://localhost',
+    'port' => 4000,
+    'api_key' => 'your_whatsapp_api_key',
+    'freescout_webhook' => 'http://freescout.drupalvm.test/whatsapp/webhook/xxx/xxx/xxx',
+  ],
+
+  /*
+    * Telegram
+    */
+  'Telegram' => [
+    'telegram_bot' => '_bot',
+    'telergam_token' => 'XXX',
+  ],
+
+  /*
      * WebSocket Configuration.
      */
-    'WebSocket' => [
-      'base_url' => 'ws://localhost',
-      'port' => 5001,
-      'vue_api_key' => 'your_vue_api_key',
-      'cake_api_key' => 'your_cake_api_key',
-    ],
-  
-    /*
+  'WebSocket' => [
+    'base_url' => 'ws://localhost',
+    'port' => 5001,
+    'vue_api_key' => 'your_vue_api_key',
+    'cake_api_key' => 'your_cake_api_key',
+  ],
+
+  /*
      * Campaigns Configuration.
-     */  
+     */
   //   'urlPrenota' => 'http://prenota.example.test',
   //   'logo' => 'https://example.com/img/logo.svg',
   //   'MailAdmin' => ['info@example.com' => 'Example'],
-    'MailLogo' => "/img/logo.png",
+  'MailLogo' => "/img/logo.png",
   //   'Trello.sales' => "https://trello.com/b/example/example-sales-pipeline",
   //   'DefaultMessage' => <<<'EOT'
   //     Dear :Name,
   //     We are pleased to invite you to an important event that will take place
   //     on <b>June 5, 2020</b> at 6 PM at our headquarters.
-  
+
   //     These are some available fields:
   //     :Name
   //     :Surname
@@ -143,9 +154,9 @@ return [
   //     :Title
   //     :Position
   //     :Company
-  
+
   //     Best regards,
   //     The team
   //   EOT,
-  
-  ];
+
+];
